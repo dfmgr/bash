f45356827ac555a3515d3392dd04b5075efd6199
casjay <git-admin@casjaysdev.com>
2020-08-26 19:58:18 -0400

    Create fzf.bash

---
+++ functions/fzf.bash
@@ -0,0 +1,6 @@
+open_with_fzf() {
+    fd -t f -H -I | fzf -m --preview="xdg-mime query default {}" | xargs -ro -d "\n" xdg-open 2>&-
+}
+cd_with_fzf() {
+    cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
+}
