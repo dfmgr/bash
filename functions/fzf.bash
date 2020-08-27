83577c36250e898cb99947f8834dba4db4b66226
casjay <git-admin@casjaysdev.com>
2020-08-26 20:08:58 -0400

    Update fzf.bash

--- functions/fzf.bash
+++ functions/fzf.bash
@@ -1,6 +1,9 @@
+__fd() { fd || fdfind ;}
+
 open_with_fzf() {
-    fd -t f -H -I | fzf -m --preview="xdg-mime query default {}" | xargs -ro -d "\n" xdg-open 2>&-
+    __fd -t f -H -I | fzf -m --preview="xdg-mime query default {}" | xargs -ro -d "\n" xdg-open 2>&-
 }
+
 cd_with_fzf() {
-    cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
+    cd $HOME && cd "$(__fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
 }
