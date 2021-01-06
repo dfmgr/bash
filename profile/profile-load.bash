3c633e8a8f88e195f517f1d0bcd82b4b2cb3f183
casjay <git-admin@casjaysdev.com>
2021-01-05 22:52:48 -0500

    Update profile-load.bash

--- profile/profile-load.bash
+++ profile/profile-load.bash
@@ -2,24 +2,36 @@
 
 case "$(uname -s)" in
 CYGWIN* | MINGW32* | MSYS* | MINGW*)
-  if ls "$HOME/.config/shell/profile/00-default.win" 1>/dev/null 2>&1; then
-    source "$HOME/.config/shell/profile/00-default.win"
-  elif ls "$HOME/.config/bash/profile/00-default.win" 1>/dev/null 2>&1; then
-    source "$HOME/.config/bash/profile/00-default.win"
+  if ls "$HOME"/.config/shell/profile/*.win 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/shell/profile/*.win; do
+      source "$f"
+    done
+  elif ls "$HOME"/.config/bash/profile/*.win 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/bash/profile/*.win; do
+      source "$f"
+    done    
   fi
   ;;
 Darwin)
-  if ls "$HOME/.config/shell/profile/00-default.mac" 1>/dev/null 2>&1; then
-    source "$HOME/.config/shell/profile/00-default.mac"
-  elif ls "$HOME/.config/bash/profile/00-default.mac" 1>/dev/null 2>&1; then
-    source "$HOME/.config/bash/profile/00-default.mac"
+  if ls "$HOME"/.config/shell/profile/*.mac 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/shell/profile/*.mac; do
+      source "$f"
+    done
+  elif ls "$HOME"/.config/bash/profile/*.mac 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/bash/profile/*.mac; do
+      source "$f"
+    done
   fi
   ;;
 Linux)
-  if ls "$HOME/.config/shell/profile/00-default.lin" 1>/dev/null 2>&1; then
-    source "$HOME/.config/shell/profile/00-default.lin"
-  elif ls "$HOME/.config/bash/profile/00-default.lin" 1>/dev/null 2>&1; then
-    source "$HOME/.config/bash/profile/00-default.lin"
+  if ls "$HOME"/.config/shell/profile/*.lin 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/shell/profile/*.lin; do
+      source "$f"
+    done
+  elif ls "$HOME"/.config/bash/profile/*.lin 1>/dev/null 2>&1; then
+    for f in "$HOME"/.config/bash/profile/*.lin; do
+      source "$f"
+    done
   fi
   ;;
 *)
