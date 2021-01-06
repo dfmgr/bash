3e0922cc8e94e27701b91a8b2208e19d51bed7d6
casjay <git-admin@casjaysdev.com>
2021-01-06 16:36:19 -0500

    Create clipboard.bash

---
+++ functions/clipboard.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @Author      : Jason
+# @Contact     : casjaysdev@casjay.net
+# @File        : clipboard.bash
+# @Created     : Mon, Jan 6, 2021, 00:00 EST
+# @License     : WTFPL
+# @Copyright   : Copyright (c) CasjaysDev
+# @Description : clipboard functions
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  printclip() { cmd_exists pbpaste && LC_CTYPE=UTF-8 tr -d "\n" | pbpaste || return 1; }
+  putclip() { cmd_exists pbcopy && LC_CTYPE=UTF-8 tr -d "\n" | pbcopy || return 1; }
+elif [[ "$OSTYPE" =~ ^linux ]]; then
+  printclip() { cmd_exists xclip && xclip -o -s || return 1; }
+  putclip() { cmd_exists xclip && xclip -i -sel c || return 1; }
+fi
