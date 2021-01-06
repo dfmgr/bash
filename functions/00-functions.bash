f5219aeb8a025206c86213c11809437b24a78e70
casjay <git-admin@casjaysdev.com>
2021-01-06 18:09:15 -0500

    Update 00-functions.bash

--- functions/00-functions.bash
+++ functions/00-functions.bash
@@ -152,6 +152,31 @@ fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+if [ ! -n "$(command -v is_online)" ]; then
+is_online() {
+  return_code() {
+    if [ "$1" = 0 ]; then
+      return 0
+    else
+      return 1
+    fi
+  }
+  test_ping() {
+    timeout 0.3 ping -c1 8.8.8.8 &>/dev/null
+    local pingExit=$?
+    return_code $pingExit
+  }
+  test_http() {
+    curl -LSIs --max-time 1 http://1.1.1.1 | grep "HTTP/2 200" | head -n 1 &>/dev/null
+    local httpExit=$?
+    return_code $httpExit
+  }
+  test_ping || test_http
+}
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 # the fuck
 
 fuck() {
