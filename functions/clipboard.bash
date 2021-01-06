2df4e917b6fda053e320031591f15dde295beab6
casjay <git-admin@casjaysdev.com>
2021-01-06 18:07:52 -0500

    Update clipboard.bash

--- functions/clipboard.bash
+++ functions/clipboard.bash
@@ -12,9 +12,9 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-  printclip() { cmd_exists pbpaste && LC_CTYPE=UTF-8 tr -d "\n" | pbpaste || return 1; }
-  putclip() { cmd_exists pbcopy && LC_CTYPE=UTF-8 tr -d "\n" | pbcopy || return 1; }
+  [[ ! -n "$(command -v printclip)" ]] && printclip() { cmd_exists pbpaste && LC_CTYPE=UTF-8 tr -d "\n" | pbpaste || return 1; }
+  [[ ! -n "$(command -v putclip)" ]] && putclip() { cmd_exists pbcopy && LC_CTYPE=UTF-8 tr -d "\n" | pbcopy || return 1; }
 elif [[ "$OSTYPE" =~ ^linux ]]; then
-  printclip() { cmd_exists xclip && xclip -o -s || return 1; }
-  putclip() { cmd_exists xclip && xclip -i -sel c || return 1; }
+  [[ ! -n "$(command -v printclip)" ]] && printclip() { cmd_exists xclip && xclip -o -s || return 1; }
+  [[ ! -n "$(command -v putclip)" ]] && putclip() { cmd_exists xclip && xclip -i -sel c || return 1; }
 fi
