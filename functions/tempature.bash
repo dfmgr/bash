afc75618310348991ab6f8893777453188bb13ca
casjay <git-admin@casjaysdev.com>
2021-01-08 04:15:51 -0500

    Update tempature.bash

--- functions/tempature.bash
+++ functions/tempature.bash
@@ -14,24 +14,24 @@
 if ! cmd_exists celcius2f f2celcius; then
   celsius2f() {
     if [ -z "$1" ] || [ $# -ne 1 ]; then
-      printf_help "Usage: celsius2f 40"
+      printf "\t\tUsage: celsius2f 40\n"
       return 1
     fi
     math="$1"
     tf=$(echo "scale=2;((9/5) * $math) + 32" | bc)
-    echo $tf
+    printf "\t\t$tf\n"
   }
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   f2celcius() {
     if [ -z "$1" ] || [ $# -ne 1 ]; then
-      printf_help "Usage: f2celcius 75"
+      printf "\t\tUsage: f2celcius 75\n"
       return 1
     fi
     math="$1"
     tc=$(echo "scale=2;(5/9)*($math-32)" | bc)
-    echo $tc
+    printf "\t\t$tf\n"
   }
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
