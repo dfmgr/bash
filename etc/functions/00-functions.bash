3324fde8010ab244634eaa9e2f9b856d1c91bda6
Jason <jason@casjay.net>
2021-01-25 05:27:27 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- etc/functions/00-functions.bash
+++ etc/functions/00-functions.bash
@@ -11,33 +11,139 @@
 #
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+# Icons
+ICON_INFO="[ ℹ️ ]"
+ICON_GOOD="[ ✔ ]"
+ICON_WARN="[ ❗ ]"
+ICON_ERROR="[ ✖ ]"
+ICON_QUESTION="[ ❓ ]"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 printf_color() { printf "%b" "$(tput setaf "$2" 2>/dev/null)" "$1" "$(tput sgr0 2>/dev/null)"; }
-printf_normal() { printf_color "\t\t$1\n" "$2"; }
+printf_normal() { printf_color "\t\t$1\n" "0"; }
 printf_green() { printf_color "\t\t$1\n" 2; }
 printf_red() { printf_color "\t\t$1\n" 1; }
 printf_purple() { printf_color "\t\t$1\n" 5; }
 printf_yellow() { printf_color "\t\t$1\n" 3; }
 printf_blue() { printf_color "\t\t$1\n" 4; }
 printf_cyan() { printf_color "\t\t$1\n" 6; }
-printf_info() { printf_color "\t\t[ ℹ️ ] $1\n" 3; }
-printf_help() { printf_color "\t\t$1\n" 1; }
-printf_read() { printf_color "\t\t$1" 5; }
-printf_success() { printf_color "\t\t[ ✔ ] $1\n" 2; }
-printf_error() { printf_color "\t\t[ ✖ ] $1 $2\n" 1; }
-printf_warning() { printf_color "\t\t[ ❗ ] $1\n" 3; }
-printf_question() { printf_color "\t\t[ ❓ ] $1 [❓] " 6; }
-answer_is_yes() { [[ "$REPLY" =~ ^[Yy]$ ]] && return 0 || return 1; }
-ask_for_confirmation() {
-  printf_question "$1"
-  read -r -n 1
-  printf ""
-
+printf_info() { printf_color "\t\t$ICON_INFO $1\n" 3; }
+printf_success() { printf_color "\t\t$ICON_GOOD $1\n" 2; }
+printf_error() { printf_color "\t\t$ICON_ERROR $1 $2\n" 1; }
+printf_warning() { printf_color "\t\t$ICON_WARN $1\n" 3; }
+printf_question() { printf_color "\t\t$ICON_QUESTION $1 " 6; }
+printf_error_stream() { while read -r line; do printf_error "↳ ERROR: $line"; done; }
+printf_execute_success() { printf_color "\t\t$ICON_ERROR $1  \n" 2; }
+printf_execute_error() { printf_color "\t\t$ICON_ERROR $1 $2 \n" 1; }
+printf_execute_result() {
+  if [ "$1" -eq 0 ]; then printf_execute_success "$2"; else printf_execute_error "$2"; fi
+  return "$1"
 }
-# use grc if it's installed or execute the command direct
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+printf_execute_error_stream() { while read -r line; do printf_execute_error "↳ ERROR: $line"; done; }
 
-if [[ ! -n "$(command -v grc)" ]]; then
-  if [[ USEGRC = "yes" ]]; then
+printf_exit() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="1"
+  local msg="$*"
+  shift
+  printf_color "\t\t$msg" "$color"
+  echo ""
+  exit $?
+}
+
+printf_help() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="4"
+  local msg="$*"
+  shift
+  echo ""
+  printf_color "\t\t$msg\n" "$color"
+  echo ""
+  exit 0
+}
+
+printf_custom() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="5"
+  local msg="$*"
+  shift
+  printf_color "\t\t$msg" "$color"
+  echo ""
+}
+
+printf_read() {
+  set -o pipefail
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="6"
+  while read line; do
+    printf_color "\t\t$line" "$color"
+  done
+  printf "\n"
+  set +o pipefail
+}
+
+printf_readline() {
+  set -o pipefail
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="6"
+  while read line; do
+    printf_color "\t\t$line\n" "$color"
+  done
+  set +o pipefail
+}
+
+printf_question() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="4"
+  local msg="$*"
+  shift
+  printf_color "\t\t$ICON_QUESTION $msg? " "$color"
+}
+
+printf_custom_question() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="1"
+  local msg="$*"
+  shift
+  printf_color "\t\t$msg " "$color"
+}
+
+printf_answer() {
+  read -e -r -n "${2:-120}" -s "${1:-__ANSWER}"
+  history -s "${1:-__ANSWER}"
+}
+
+#printf_read_question "color" "message" "maxLines" "answerVar"
+printf_read_question() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="1"
+  local msg="$1" && shift 1
+  local lines="${1:-120}" && shift 1
+  local reply="${1:-__ANSWER}" && shift 1
+  printf_color "\t\t$msg " "$color"
+  printf_answer "$reply" "$lines"
+}
+
+printf_answer_yes() { [[ "${1:-__ANSWER}" =~ ${2:-^[Yy]$} ]] && return 0 || return 1; }
+
+printf_head() {
+  test -n "$1" && test -z "${1//[0-9]/}" && local color="$1" && shift 1 || local color="6"
+  local msg1="$1" && shift 1
+  local msg2="$1" && shift 1 || msg2=
+  local msg3="$1" && shift 1 || msg3=
+  local msg4="$1" && shift 1 || msg4=
+  local msg5="$1" && shift 1 || msg5=
+  local msg6="$1" && shift 1 || msg6=
+  local msg7="$1" && shift 1 || msg7=
+  shift
+  [ -z "$msg1" ] || printf_color "\t\t##################################################\n" "$color"
+  [ -z "$msg1" ] || printf_color "\t\t$msg1\n" "$color"
+  [ -z "$msg2" ] || printf_color "\t\t$msg2\n" "$color"
+  [ -z "$msg3" ] || printf_color "\t\t$msg3\n" "$color"
+  [ -z "$msg4" ] || printf_color "\t\t$msg4\n" "$color"
+  [ -z "$msg5" ] || printf_color "\t\t$msg5\n" "$color"
+  [ -z "$msg6" ] || printf_color "\t\t$msg6\n" "$color"
+  [ -z "$msg7" ] || printf_color "\t\t$msg7\n" "$color"
+  [ -z "$msg1" ] || printf_color "\t\t##################################################\n" "$color"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# use grc if it's installed or execute the command direct
+
+if [[ -z "$(command -v grc)" ]]; then
+  if [[ "$USEGRC" = "yes" ]]; then
     grc() {
       if [[ -f "$(command -v grc)" ]]; then
         #grc --colour=auto
@@ -52,20 +158,19 @@ fi
 #
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # generate random strings
 
-if [[ ! -n "$(command -v random-string)" ]]; then
+if [[ -z "$(command -v random-string)" ]]; then
   random-string() {
-    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "${1:-64}" | head -n 1
   }
 fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-if [[ ! -n "$(command -v mkpasswd)" ]]; then
+if [[ -z "$(command -v mkpasswd)" ]]; then
   mkpasswd() {
-    cat /dev/urandom | tr -dc [:print:] | tr -d '[:space:]\042\047\134' | fold -w ${1:-64} | head -n 1
+    cat /dev/urandom | tr -dc [:print:] | tr -d '[:space:]\042\047\134' | fold -w "${1:-64}" | head -n 1
   }
 fi
 
