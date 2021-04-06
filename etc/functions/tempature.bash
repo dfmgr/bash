0347b5889d7cf521a5afe04dc5da07ab937d0ef7
Jason <jason@casjay.net>
2021-04-06 04:51:48 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- etc/functions/tempature.bash
+++ etc/functions/tempature.bash
@@ -13,26 +13,24 @@
 # @Other         :
 # @Resource      :
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if ! cmd_exists celcius2f f2celcius; then
-  celcius2f() {
-    if [ -z "$1" ] || [ $# -ne 1 ]; then
-      printf "\t\tUsage: celcius2f 40\n"
-      return 1
-    fi
-    math="$1"
-    tf=$(echo "scale=2;((9/5) * $math) + 32" | bc)
-    printf "\t\t$tf\n"
-  }
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  f2celcius() {
-    if [ -z "$1" ] || [ $# -ne 1 ]; then
-      printf "\t\tUsage: f2celcius 75\n"
-      return 1
-    fi
-    math="$1"
-    tc=$(echo "scale=2;(5/9)*($math-32)" | bc)
-    printf "\t\t$tf\n"
-  }
-fi
+celcius2f() {
+  if [ -z "$1" ] || [ $# -ne 1 ]; then
+    printf "\t\tUsage: celcius2f 40\n"
+    return 1
+  fi
+  math="$1"
+  tf=$(echo "scale=2;((9/5) * $math) + 32" | bc)
+  printf "\t\t$tf\n"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+f2celcius() {
+  if [ -z "$1" ] || [ $# -ne 1 ]; then
+    printf "\t\tUsage: f2celcius 75\n"
+    return 1
+  fi
+  math="$1"
+  tc=$(echo "scale=2;(5/9)*($math-32)" | bc)
+  printf "\t\t$tf\n"
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
