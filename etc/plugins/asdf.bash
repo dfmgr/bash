29571530989078a76287fe21ceadc8b7cdce2181
Jason <jason@casjay.net>
2021-07-28 15:25:57 -0400

    🐛 FIXED: etc/plugins/asdf.bash 🐛

--- etc/plugins/asdf.bash
+++ etc/plugins/asdf.bash
@@ -2,10 +2,15 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # asdf
 if [ -n "$BASH_VERSION" ]; then
-  if [ -f ~/.local/share/asdf/asdf.sh ]; then
-    . ~/.local/share/asdf/asdf.sh
+  if [ -d "$HOME/.local/share/misc/plugins/asdf" ]; then
+    ASDF_DIR="$HOME/.local/share/misc/plugins/asdf"
+    if [ -f "$ASDF_DIR/asdf.sh" ]; then
+      . "$ASDF_DIR/asdf.sh"
+    fi
+    if [ -f "$ASDF_DIR/completions/asdf.bash" ]; then
+      . "$ASDF_DIR/completions/asdf.bash"
+    fi
   fi
-
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
