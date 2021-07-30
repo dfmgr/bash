8660c2d0864f206d4d32916dad71eee112b0170a
Jason <jason@casjay.net>
2021-07-30 17:42:22 -0400

    🐛 FIXED: etc/prompt/01-powerline.bash 🐛

--- etc/prompt/01-powerline.bash
+++ etc/prompt/01-powerline.bash
@@ -438,7 +438,7 @@ bashprompt() {
     [ -n "$(command -v lua 2>/dev/null)" ] && PS1+="$BG_MAGENTA$FG_BLACK$(__iflua && __lua_info)$RESET"
     [ -n "$(command -v git 2>/dev/null)" ] && PS1+="$BG_CYAN$FG_BLACK$(__ifgit && __git_info)$RESET"
     PS1+="$BG_PURPLE$FG_BLACK${PS_TIME}$RESET "
-    PS1+="$BG_GRAY2$FG_BLACK\u@\H :$BG_DARK_GREEN\w :$RESET$(__additional_msg)\n"
+    PS1+="$BG_GRAY2$FG_BLACK\u@\H: $BG_DARK_GREEN\w: $RESET$(__additional_msg)\n"
     PS1+="$BG_EXIT${FG_BLACK}Jobs:[\j]$BG_GRAY1${PS1_ADD_PROMPT:-}$PS_SYMBOL:$RESET "
   }
   PROMPT_COMMAND="ps1 && title && history -a && history -r "
