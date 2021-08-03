2ce9f5a4fbef039c9e3d96f164a6641edb0a3dc4
Jason <jason@casjay.net>
2021-08-03 03:12:58 -0400

    ❓ Added: etc/post/  ❓

---
+++ etc/post/fnm.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202108030255-git
+# @Author        : casjay
+# @Contact       : casjay
+# @License       : LICENSE.md
+# @ReadME        : fnm.bash --help
+# @Copyright     : Copyright: (c) 2021 casjay, casjay
+# @Created       : Tuesday, Aug 03, 2021 03:05 EDT
+# @File          : fnm.bash
+# @Description   : fast node manager
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+command -v fnm &>/dev/null && eval "$(fnm env)"
