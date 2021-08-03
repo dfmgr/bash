cc7a3878ceb1703ebf2165af44f2010b54142f5f
Jason <jason@casjay.net>
2021-08-03 03:48:07 -0400

    ❓ Added: etc/post/zoxide.bash  ❓

---
+++ etc/post/zoxide.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202108030344-git
+# @Author        : Jason
+# @Contact       : jason@casjaysdev.com
+# @License       : WTFPL
+# @ReadME        : zoxide --help
+# @Copyright     : Copyright (c) 2021, Casjays Developments
+# @Created       : Tuesday Aug 03, 2021 03:45:21 EDT
+# @File          : zoxide
+# @Description   :
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+command -v zoxide &>/dev/null && eval "$(zoxide init bash)"
+
