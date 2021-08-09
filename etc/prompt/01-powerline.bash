c2728cfe20bb70788550b15cee176dc2ca13b67e
casjay <git-admin@casjaysdev.com>
2021-08-09 00:03:10 -0400

    Update 01-powerline.bash

--- etc/prompt/01-powerline.bash
+++ etc/prompt/01-powerline.bash
@@ -415,9 +415,9 @@ bashprompt() {
       local PS_SYMBOL=" 😔 "
     fi
     PS_LINE="$(printf -- '%.0s' {4..2000})"
-    PS_FILL="${PS_LINE:0:$COLUMNS}"
+    PS_FILL="${PS_LINE:0:$((COLUMNS-1))}"
     PS_TIME="\[\033[\$((COLUMNS-10))G\]${RESET}${BG_PURPLE}${FG_BLACK}[\t]$RESET"
-    PS1="\${PS_FILL}\[\033[0G\]$RESET "
+    PS1="\${PS_FILL}\[\033[0G\]$RESET"
     PS1+="$BG_BLUE$FG_BLACK\s: \v $RESET"
     PS1+="$BG_PURPLE$FG_GRAY1$(__ifphp && __php_info)$RESET"
     PS1+="$BG_DARK_RED$FG_GRAY1$(__ifruby && __ruby_info)$RESET"
@@ -426,7 +426,7 @@ bashprompt() {
     PS1+="$BG_PURPLE$FG_BLACK$(__ifperl && __perl_info)$RESET"
     PS1+="$BG_MAGENTA$FG_BLACK$(__iflua && __lua_info)$RESET"
     PS1+="$BG_CYAN$FG_BLACK$(__ifgit && __git_info)$RESET"
-    PS1+="$BG_PURPLE$FG_BLACK${PS_TIME}$RESET"
+    PS1+="$BG_PURPLE$FG_BLACK${PS_TIME}$RESET\n"
     PS1+="$BG_GRAY2$FG_BLACK\u@\H: $BG_DARK_GREEN\w:$RESET$(__additional_msg)\n"
     PS1+="$BG_EXIT${FG_BLACK}Jobs:[\j]$BG_GRAY1${PS1_ADD_PROMPT:-}$PS_SYMBOL:$RESET "
   }
