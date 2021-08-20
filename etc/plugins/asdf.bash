75c797d389c7f96e214436ab96a8437514fccd9a
casjay <git-admin@casjaysdev.com>
2021-08-20 13:03:06 -0400

    Update asdf.bash

--- etc/plugins/asdf.bash
+++ etc/plugins/asdf.bash
@@ -15,14 +15,9 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # asdf
 if [ -n "$BASH_VERSION" ]; then
-  if [ -d "$HOME/.local/share/misc/plugins/asdf" ]; then
-    ASDF_DIR="$HOME/.local/share/misc/plugins/asdf"
-    if [ -f "$ASDF_DIR/asdf.sh" ]; then
-      . "$ASDF_DIR/asdf.sh"
-    fi
-    if [ -f "$ASDF_DIR/completions/asdf.bash" ]; then
-      . "$ASDF_DIR/completions/asdf.bash"
-    fi
+  if [ -f "$ASDF_DIR/asdf" ]; then
+    . "$ASDF_DIR/asdf.sh"
+    . "$ASDF_DIR/completions/asdf.bash"
   fi
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
