b16dc0b1c1835c939e47411e24a1a895b8487f17
casjay <git-admin@casjaysdev.com>
2021-08-20 11:41:42 -0400

    Create luaver.bash

---
+++ etc/plugins/luaver.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202108030255-git
+# @Author        : casjay
+# @Contact       : casjay
+# @License       : LICENSE.md
+# @ReadME        : luaver.bash --help
+# @Copyright     : Copyright: (c) 2021 casjay, casjay
+# @Created       : Tuesday, Aug 03, 2021 03:05 EDT
+# @File          : luaver.bash
+# @Description   : lua version manager
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup luaver
+[ -s "$LUAVER_HOME/luaver" ] && . "$LUAVER_HOME/luaver"
+[ -s "$LUAVER_HOME/completions/luaver.bash" ] && . "$LUAVER_HOME/completions/luaver.bash"
