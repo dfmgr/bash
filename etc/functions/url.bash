4a6059034dd342a5e82cfe00d681b0e79ca5b28d
casjay <git-admin@casjaysdev.com>
2021-08-21 17:08:06 -0400

    Update url.bash

--- etc/functions/url.bash
+++ etc/functions/url.bash
@@ -13,28 +13,28 @@
 # @Other         :
 # @Resource      :
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-urlencode() {
-  # needs liburi-perl to be installed
-  local url="$1"
-  [[ -z "$url" ]] && url=$(cat -)
-  perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$url"
-  echo ""
-}
+#urlencode() {
+#  # needs liburi-perl to be installed
+#  local url="$1"
+#  [[ -z "$url" ]] && url=$(cat -)
+#  perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$url"
+#  echo ""
+#}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-urldecode() {
-  # needs liburi-perl to be installed
-  local url="$1"
-  [[ -z "$url" ]] && url=$(cat -)
-  perl -MURI::Escape -e 'print uri_unescape($ARGV[0]);' "$url"
-  echo ""
-}
+#urldecode() {
+#  # needs liburi-perl to be installed
+#  local url="$1"
+#  [[ -z "$url" ]] && url=$(cat -)
+#  perl -MURI::Escape -e 'print uri_unescape($ARGV[0]);' "$url"
+#  echo ""
+#}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-expandurl() {
-  local url=$1
-  [[ -z "$url" ]] && url=$(printclip)
-  [[ -z "$url" ]] && echo "Nothing to expand" && return 1
-  wget -S "$url" 2>&1 | grep ^Location | awk '{print $2}' | tee >(putclip)
-}
+#expandurl() {
+#  local url=$1
+#  [[ -z "$url" ]] && url=$(printclip)
+#  [[ -z "$url" ]] && echo "Nothing to expand" && return 1
+#  wget -S "$url" 2>&1 | grep ^Location | awk '{print $2}' | tee >(putclip)
+#}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
