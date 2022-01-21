a0dc4107ebf791b9cee928d578f645d35f0fc329
Jason <git-admin@casjaysdev.com>
2022-01-21 18:42:50 -0500

    🗃  Modified: etc/functions/latest-iso.bash 🗃

--- etc/functions/latest-iso.bash
+++ etc/functions/latest-iso.bash
@@ -15,16 +15,22 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 latest-iso() {
   local iso url latest download_dir
-  url="$1"
-  latest=$(curl -q -LSsf "$url" | grep 'http*.*.*iso' | sed 's|\\/|/|g' | head -n1)
-  iso=$(basename "$latest")
-  download_file="${2:-$HOME/Downloads}/$iso"
-  download_dir=$(dirname "$download_file")
+  [[ "$1" = "tails" ]] && shift 1 && url="https://tails.boum.org/install/dvd-download/index.en.html"
+  url="${1:-$url}"
+  file="${2:-$file}"
+  latest=$(curl -q -LSsf "$url" 2>/dev/null | sed -r 's/.*href="([^"]+).*/\1/g' | grep 'http*.*.*iso' | grep -v 'torrent' | grep 'http' | sed 's|\\/|/|g' | head -n1 | grep '^')
+  echo "$latest"
+  return 1
+  [ -n "$latest" ] || printf_return "No iso found on $url"
+  iso=$(basename "$latest" 2>/dev/null)
+  download_dir=$(dirname "${2:-$HOME/Downloads}/$iso" 2>/dev/null)
+  download_file="$download_dir/$iso"
+  [ -e "$download_file" ] && printf_purple "File already exists: $download_file" && return 0
   [ -d "$download_dir" ] || mkdir -p "$download_dir"
-  printf_green "Downloading $iso from $url"
-  curl -q -LSsf "$latest" -o "$download_file" &&
+  printf_green "Downloading $iso to $download_file"
+  curl -q -LSsf "$latest" -o "$download_file" 2>/dev/null &&
     printf_green "File saved to $download_file" || {
-    printf_red "Error downloading $latest"
+    printf_red "Error downloading $url"
     return 1
   }
 }
