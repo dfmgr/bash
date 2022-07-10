15269cd96e3316c36ceb1127d6801b2981f1c32e
Jason <git-admin@casjaysdev.com>
2022-07-09 22:21:54 -0400

    🗃️  modified: completions/brew-sh.bash 🗃️

--- completions/brew-sh.bash
+++ completions/brew-sh.bash
@@ -18,10 +18,11 @@ if type brew &>/dev/null; then
   if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
     . "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
   else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d"/*; do
       [[ -r "$COMPLETION" ]] && . "$COMPLETION"
     done
   fi
 fi
+unset COMPLETION
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
