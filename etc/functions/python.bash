84bfc7862084c757ce2c27aa9884a8ce20ffd90d
Jason <git-admin@casjaysdev.com>
2022-07-09 22:22:12 -0400

    🗃️  modified: etc/functions/python.bash 🗃️

--- etc/functions/python.bash
+++ etc/functions/python.bash
@@ -17,12 +17,13 @@ getpythonver() {
   if [[ "$(python3 -V 2>/dev/null)" =~ "Python 3" ]]; then
     PYTHONVER="python3"
     PIP="pip3"
-    export PATH="${PATH}:$(python3 -c 'import site; print(site.USER_BASE)')/bin"
+    PATH="${PATH}:$(python3 -c 'import site; print(site.USER_BASE)')/bin"
   elif [[ "$(python2 -V 2>/dev/null)" =~ "Python 2" ]]; then
     PYTHONVER="python"
     PIP="pip"
-    export PATH="${PATH}:$(python -c 'import site; print(site.USER_BASE)')/bin"
+    PATH="${PATH}:$(python -c 'import site; print(site.USER_BASE)')/bin"
   fi
+  export PATH
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
