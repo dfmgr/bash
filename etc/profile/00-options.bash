d4865bbe14c5aa09912f03699da9c4d1e59ea26d
Jason <git-admin@casjaysdev.com>
2022-07-09 22:22:17 -0400

    🗃️  modified: etc/profile/00-options.bash 🗃️

--- etc/profile/00-options.bash
+++ etc/profile/00-options.bash
@@ -14,7 +14,7 @@
 # @Resource      : https://www.gnu.org/software/bash/manual/bashref.html#The-Shopt-Builtin
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # XSet options
-if command -v xset >/dev/null 2>&1; then
+if command -v xset &>/dev/null; then
   xset s off 2>/dev/null
   xset -dpms 2>/dev/null
   xset s off -dpms 2>/dev/null
@@ -49,8 +49,8 @@ shopt -s no_empty_cmd_completion 2>/dev/null
 # Match filenames in a case-insensitive fashion when performing filename expansion.
 shopt -s nocaseglob 2>/dev/null
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# expand aliases
-shopt -s expand_aliases 2>/dev/null
+# expand aliases -u disable, -s enable
+shopt -u expand_aliases 2>/dev/null
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set mouse type - changes to blinking bar
 echo -e -n "\x1b[\x35 q"
