87353a557e743a8970995b36f70175279264bcae
casjay <git-admin@casjaysdev.com>
2023-04-25 10:26:05 -0400

    🗃️ Committing everything that changed 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
-# shellcheck disable=SC2317
-# shellcheck disable=SC2120
-# shellcheck disable=SC2155
-# shellcheck disable=SC2199
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202304250248-git
+##@Version           :  202304250253-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.com
-# @@License          :  WTFPL
+# @@License          :  LICENSE.md
 # @@ReadME           :  install.sh --help
 # @@Copyright        :  Copyright: (c) 2023 Jason Hempstead, Casjays Developments
-# @@Created          :  Tuesday, Apr 25, 2023 02:48 EDT
+# @@Created          :  Tuesday, Apr 25, 2023 10:20 EDT
 # @@File             :  install.sh
 # @@Description      :  Install configurations for bash
 # @@Changelog        :  New script
@@ -22,8 +18,14 @@
 # @@sudo/root        :  no
 # @@Template         :  installers/dfmgr
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# shell check options
+# shellcheck disable=SC2317
+# shellcheck disable=SC2120
+# shellcheck disable=SC2155
+# shellcheck disable=SC2199
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="bash"
-VERSION="202304250248-git"
+VERSION="202304250253-git"
 HOME="${USER_HOME:-$HOME}"
 USER="${SUDO_USER:-$USER}"
 RUN_USER="${SUDO_USER:-$USER}"
@@ -56,26 +58,34 @@ else
   exit 90
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Define pre-install scripts
-__run_pre_install() {
-
-  return ${?:-0}
-}
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Define custom functions
-
+__download_file() { curl -q -LSsf "$1" -o "$2" || return 1; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Requires root - no point in continuing
+#sudoreq "$0 *" # sudo required
+#sudorun "$0 *" # sudo optional
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Make sure the scripts repo is installed
+scripts_check
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Call the main function
 dfmgr_install
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script options IE: --help --version
+show_optvars "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # trap the cleanup function
 trap_exit
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Initialize the installer
+dfmgr_run_init
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # OS Support: supported_os unsupported_oses
+supported_os linux mac windows
 unsupported_oses
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Make sure the scripts repo is installed
-scripts_check
+# Do not update
+#installer_noupdate "$@"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Defaults
 APPNAME="${APPNAME:-install.sh}"
@@ -90,121 +100,47 @@ APPVERSION="$(__appversion "$REPORAW/version.txt")"
 PLUGIN_REPOS="https://github.com/Bash-it/bash-it"
 PLUGIN_DIR="${SHARE:-$HOME/.local/share}/$APPNAME/plugins"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Export variables
-
+# Specufy custom package name
+PKG="$APPNAME"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Require a version higher than
-dfmgr_req_version "$APPVERSION"
+# define arch user repo packages
+if if_os_id arch; then
+  AUR=""
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Script options IE: --help --version
-show_optvars "$@"
+# define linux packages
+if if_os linux; then
+  APP="$PKG "
+  if_os_id arch && APP+=""
+  if_os_id centos && APP+=""
+  if_os_id debian && APP+=""
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Do not update
-#installer_noupdate "$@"
+# Define MacOS packages - homebrew
+if if_os mac; then
+  APP="$PKG "
+  APP+=""
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Requires root - no point in continuing
-#sudoreq "$0 *" # sudo required
-#sudorun "$0 *" # sudo optional
+# Define Windows packages - choco
+if if_os win; then
+  APP="$PKG "
+  APP+=""
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Other dependencies
-dotfilesreq misc
-dotfilesreqadmin cron
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Initialize the installer
-dfmgr_run_init
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Run pre-install commands
-execute "__run_pre_install" "Running pre-installation commands"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# end with a space
-APP="$APPNAME "
-AUR=""
+# define packages
 PERL=""
 PYTH=""
 PIPS=""
 CPAN=""
 GEMS=""
 NPM=""
+PHP=""
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# install required packages using the aur - Requires yay to be installed
-install_aur "$AUR"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# install packages - useful for package that have the same name on all oses
-install_packages "$APP"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# install required packages using file
-install_required "$APP"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for perl modules and install using system package manager
-install_perl "$PERL"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for python modules and install using system package manager
-install_python "$PYTH"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for pip binaries and install using python package manager
-install_pip "$PIPS"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for cpan binaries and install using perl package manager
-install_cpan "$CPAN"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for ruby binaries and install using ruby package manager
-install_gem "$GEMS"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# check for npm binaries and install using node package manager
-install_npm "$NPM"
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Other dependencies
-dotfilesreq
-dotfilesreqadmin
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Ensure directories exist
-ensure_dirs
-ensure_perms
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Backup if needed
-if [ -d "$APPDIR" ]; then
-  execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
-fi
-# Main progam
-if __am_i_online; then
-  if [ -d "$INSTDIR/.git" ]; then
-    execute "git_update $INSTDIR" "Updating $APPNAME configurations"
-  else
-    execute "git_clone $REPO $INSTDIR" "Installing $APPNAME configurations"
-  fi
-  # exit on fail
-  failexitcode $? "Git has failed"
-fi
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Custom plugin function
-__custom_plugin() {
-  local exitCodeC=0
-
-  return $exitCodeC
+# Define pre-install scripts
+__run_pre_install() {
+  return 0
 }
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Install Plugins
-if __am_i_online; then
-  if [ "$PLUGIN_REPOS" != "" ]; then
-    exitCodeP=0
-    [ -d "$PLUGIN_DIR" ] || mkdir -p "$PLUGIN_DIR"
-    for plugin in $PLUGIN_REPOS; do
-      plugin_name="$(basename "$plugin")"
-      plugin_dir="$PLUGIN_DIR/$plugin_name"
-      if [ -d "$plugin_dir/.git" ]; then
-        execute "git_update $plugin_dir" "Updating plugin $plugin_name"
-        [ $? -ne 0 ] && exitCodeP=$(($? + exitCodeP)) && printf_red "Failed to update $plugin_name"
-      else
-        execute "git_clone $plugin $plugin_dir" "Installing plugin $plugin_name"
-        [ $? -ne 0 ] && exitCodeP=$(($? + exitCodeP)) && printf_red "Failed to install $plugin_name"
-      fi
-    done
-  fi
-  __custom_plugin
-  exitCodeP=$(($? + exitCodeP))
-  # exit on fail
-  failexitcode $exitCodeP "Installation of plugin failed"
-fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # run before primary post install function
 __run_prepost_install() {
@@ -239,6 +175,97 @@ __run_post_install() {
   history -r &>/dev/null && history -a &>/dev/null && history -w &>/dev/null && history -r &>/dev/null && history -a &>/dev/null
   return 0
 }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Custom plugin function
+__custom_plugin() {
+  local exitCodeC=0
+  # execute "git_clone $repo $dir" "Installing plugin name"
+  return $exitCodeC
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Other dependencies
+dotfilesreq misc
+dotfilesreqadmin cron
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# END OF CONFIGURATION
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Require a version higher than
+dfmgr_req_version "$APPVERSION"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Run pre-install commands
+execute "__run_pre_install" "Running pre-installation commands"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install required packages using the aur - Requires yay to be installed
+install_aur "$AUR"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install packages - useful for package that have the same name on all oses
+install_packages "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install required packages using file
+install_required "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for perl modules and install using system package manager
+install_perl "$PERL"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for python modules and install using system package manager
+install_python "$PYTH"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for pip binaries and install using python package manager
+install_pip "$PIPS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for cpan binaries and install using perl package manager
+install_cpan "$CPAN"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for ruby binaries and install using ruby package manager
+install_gem "$GEMS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for npm binaries and install using node package manager
+install_npm "$NPM"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for php binaries and install using php composer
+install_php "$PHP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ensure directories exist
+ensure_dirs
+ensure_perms
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Backup if needed
+if [ -d "$APPDIR" ]; then
+  execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
+fi
+# Main progam
+if __am_i_online; then
+  if [ -d "$INSTDIR/.git" ]; then
+    execute "git_update $INSTDIR" "Updating $APPNAME configurations"
+  else
+    execute "git_clone $REPO $INSTDIR" "Installing $APPNAME configurations"
+  fi
+  # exit on fail
+  failexitcode $? "Git has failed"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Plugins
+if __am_i_online; then
+  if [ "$PLUGIN_REPOS" != "" ]; then
+    exitCodeP=0
+    [ -d "$PLUGIN_DIR" ] || mkdir -p "$PLUGIN_DIR"
+    for plugin in $PLUGIN_REPOS; do
+      plugin_name="$(basename "$plugin")"
+      plugin_dir="$PLUGIN_DIR/$plugin_name"
+      if [ -d "$plugin_dir/.git" ]; then
+        execute "git_update $plugin_dir" "Updating plugin $plugin_name"
+        [ $? -ne 0 ] && exitCodeP=$(($? + exitCodeP)) && printf_red "Failed to update $plugin_name"
+      else
+        execute "git_clone $plugin $plugin_dir" "Installing plugin $plugin_name"
+        [ $? -ne 0 ] && exitCodeP=$(($? + exitCodeP)) && printf_red "Failed to install $plugin_name"
+      fi
+    done
+  fi
+  __custom_plugin
+  exitCodeP=$(($? + exitCodeP))
+  # exit on fail
+  failexitcode $exitCodeP "Installation of plugin failed"
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # run post install scripts
 run_postinst() {
